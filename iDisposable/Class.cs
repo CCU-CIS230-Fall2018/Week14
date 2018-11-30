bd6117307785f334dd65d1e6006554acc31919dc
AbrilMunoz <AbrilMunoz@DESKTOP-A4HDJB9>
2018-11-29 19:42:07 -0700

    Done I Think

---
+++ iDisposable/Class.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iDisposable
+{
+    class Class : IDisposable
+    {
+        TextReader tr = null;
+
+        public Class(string path)
+        {
+            Console.WriteLine("Getting Managed Resources");
+            tr = new StreamReader(path);
+            
+            Console.WriteLine("Getting Unmanaged Resources");
+        }
+
+        void ReleaseManagedResources()
+        {
+            Console.WriteLine("Releasing Managed Resources");
+            if (tr != null)
+            {
+                tr.Dispose();
+            }
+        }
+
+        void ReleaseUnmangedResources()
+        {
+            Console.WriteLine("Releasing Unmanaged Resources");
+        }
+
+        public void ShowData()
+        {
+            if (tr != null)
+            {
+                Console.WriteLine(tr.ReadToEnd() + " / Some unmanaged data. ");
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose called from outside");
+            Dispose(true);
+            
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            Console.WriteLine("Actual Dispose called with a " + disposing.ToString());
+            if (disposing == true)
+            {
+                ReleaseManagedResources();
+            }
+            else
+            {
+            }
+            
+            ReleaseUnmangedResources();
+
+        }
+
+        ~Class()
+        {
+            Console.WriteLine("Finalizer called");
+            Dispose(false);
+        }
+    }
+}
---
+++ iDisposable/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader tr = null;
+
+            try
+            {            
+                tr = new StreamReader(@"Files\test.txt");
+                
+                string s = tr.ReadToEnd();
+
+                Console.WriteLine(s);
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                if (tr != null)
+                {
+                    tr.Dispose();
+                }
+            }
+            Console.WriteLine(); Console.WriteLine();
+            
+            TextReader tr2 = null;
+              
+            try
+            {
+                using (tr2 = new StreamReader(@"Files\test.txt"))
+                {
+                    string s = tr2.ReadToEnd();
+
+                    Console.WriteLine(s);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            Console.WriteLine(); Console.WriteLine();
+
+            Class r = null;
+
+            try
+            {
+                r = new Class(@"Files\test.txt");
+                r.ShowData();
+            }
+            finally
+            {
+                r.Dispose();
+            }
+
+            Console.WriteLine(); Console.WriteLine();
+            
+            Class r2 = null;
+            using (r2 = new Class(@"Files\test.txt"))
+            {
+                r2.ShowData();
+            }
+
+            Console.WriteLine(); Console.WriteLine();
+            
+            Class r3 = new Class(@"Files\test.txt");
+            r3.ShowData();
+
+
+
+        }
+    }
+}
+
